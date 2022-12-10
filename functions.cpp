37367c1f081e88a2e4bf59dc6294b408f696d0eb
NazakatUmrani <unazakat70@gmail.com>
2022-12-10 14:33:19 +0500

    Some Exceptions in arthimetic Functions, Problems fixed, tested

--- functions.cpp
+++ functions.cpp
@@ -20,109 +20,106 @@ string otoh(string d);
 string htob(string d);
 string htoo(string d);
 
-string string_divisor(string, int);
-string string_remainder(string, int);
-string string_multiplication(string, string);
-string string_power(string, string);
+string string_divisor(string, string);
+string string_remainder(string, string);
 string string_plus(string, string);
 string string_minus(string, string);
+string string_multiplication(string, string);
+string string_power(string, string);
+string string_max(string, string);
+string string_trimmer(string);
 //Function Declarations -- Start
 
 //Function Definations -- Start
 
 //Big Numbers Arithmetic Functions -- Start
-string string_divisor(string num, int base){
-    string quotient, substrOfNum, remainder;
-    for (int l = 0; l < num.length(); l++)
-    {
-        substrOfNum = remainder + num[l];
-        quotient = quotient + to_string(stoi(substrOfNum)/base);
-        if (quotient == "0")
-        {
-            quotient = "";
-        }
-        remainder = to_string(stoi(substrOfNum)%base);
-    }
-    return quotient;
-}
-
-string string_remainder(string num, int base){
-    string quotient, substrOfNum, remainder;
-    for (int k = 0; k < num.length(); k++)
-    {
-        substrOfNum = remainder + num[k];
-        quotient = quotient + to_string(stoi(substrOfNum)/base);
-        if (quotient == "0")
-        {
-            quotient = "";
-        }
-        remainder = to_string(stoi(substrOfNum)%base);
-    }
-    return remainder;
-}
-
 string string_plus(string str1, string str2){
     int length, zeroscount, str1len, str2len;
-    str1len = str1.length();
-    str2len = str2.length();
     string result, carry, str1reverse, str2reverse;
-    if (str2len < str1len)
+    if (str1 == "0" || str2 == "0")
     {
-        length = str1len;
-        zeroscount = (length-str2len);
-        for (int w = 0; w < zeroscount; w++)
+        if (str1 == "0")
         {
-            str2 = "0" + str2;
+            result = str2;
+        }
+        else if (str2 == "0")
+        {
+            result = str1;
         }
     }
-    else if (str1len < str2len)
-    {
-        length = str2len;
-        zeroscount = (length-str1len);
-        for (int o = 0; o < zeroscount; o++)
+    else{
+        str1len = str1.length();
+        str2len = str2.length();
+        if (str2len < str1len)
         {
-            str1 = "0" + str1;
+            length = str1len;
+            zeroscount = (length-str2len);
+            for (int w = 0; w < zeroscount; w++)
+            {
+                str2 = "0" + str2;
+            }
         }
-    }
-    else
-    {
-        length = str1.length();
-    }
-    reverse(str1.begin(), str1.end());
-    reverse(str2.begin(), str2.end());   
-    carry = "0";
-    for (int o = 0; o < length; o++)
-    {
-        string temp1, temp2, result_temp;
-        temp1 = str1[o];
-        temp2 = str2[o];
-        result_temp = to_string(stoi(carry) + stoi(temp1) + stoi(temp2));
-        if (result_temp.length() > 1)
+        else if (str1len < str2len)
         {
-            carry = result_temp[0];
-            result += result_temp[1];
+            length = str2len;
+            zeroscount = (length-str1len);
+            for (int o = 0; o < zeroscount; o++)
+            {
+                str1 = "0" + str1;
+            }
         }
         else
         {
-            carry = "0";
-            result += result_temp;
+            length = str1.length();
         }
-        if (o==(length-1) && carry != "" && carry != "0")
+        reverse(str1.begin(), str1.end());
+        reverse(str2.begin(), str2.end());   
+        carry = "0";
+        for (int o = 0; o < length; o++)
         {
-            result += carry;
-        }
-    }    
-    reverse(result.begin(), result.end());
+            string temp1, temp2, result_temp;
+            temp1 = str1[o];
+            temp2 = str2[o];
+            result_temp = to_string(stoi(carry) + stoi(temp1) + stoi(temp2));
+            if (result_temp.length() > 1)
+            {
+                carry = result_temp[0];
+                result += result_temp[1];
+            }
+            else
+            {
+                carry = "0";
+                result += result_temp;
+            }
+            if (o==(length-1) && carry != "" && carry != "0")
+            {
+                result += carry;
+            }
+        }    
+        reverse(result.begin(), result.end());
+    }
     return result;
 }
 
 string string_multiplication(string number, string multiplier){
+    string result;
     if (number == "0" || multiplier == "0")
     {
-        return "0";
+        result = "0";
+    }
+    else if (number == "1" || multiplier == "1")
+    {
+        if (number == "1")
+        {
+            result = multiplier;
+        }
+        else if (multiplier == "1")
+        {
+            result = number;
+        }
     }
     else{
-        string ans = "0", result;
+        string ans = "0";
         int carry;
         reverse(number.begin(), number.end());
         reverse(multiplier.begin(),multiplier.end());
@@ -160,102 +157,189 @@ string string_multiplication(string number, string multiplier){
             }
             ans = string_plus(ans, result);
         }
-        return ans;
+        result = ans;
     }
+    return result;
 }
 
 string string_power(string number, string power){
     string i = "1", result = number;
-    while (i != power && stoi(power) > 0)
-    {
-        result = string_multiplication(result,number);
-        i = string_plus(i, "1");
-    }
     if (power == "0")
     {
-        return "1";
+        result = "1";
+    }    
+    else{
+        while (i != power && string_max(power,"0") == power)
+        {
+            result = string_multiplication(result,number);
+            i = string_plus(i, "1");
+        }
     }
-    return result;
+    return string_trimmer(result);
 }
 
 string string_minus(string str1, string str2){
-    int length, zeroscount, str1len, str2len;
-    str1len = str1.length();
-    str2len = str2.length();
-    string result, str1reverse, str2reverse;
-    if (str2len < str1len)
+    string count = "0";
+    str1 = string_trimmer(str1);
+    str2 = string_trimmer(str2);
+    if (str1 == str2)
     {
-        length = str1len;
-        zeroscount = (length-str2len);
-        for (int w = 0; w < zeroscount; w++)
+        return "0";
+    }
+    else if (str1 == "0" || str2 == "0")
+    {
+        if (str1 == "0")
         {
-            str2 = "0" + str2;
+            count = str2;
+        }
+        else if (str2 == "0")
+        {
+            count = str1;
         }
     }
-    else if (str1len < str2len)
-    {
-        length = str2len;
-        zeroscount = (length-str1len);
-        for (int o = 0; o < zeroscount; o++)
-        {
-            str1 = "0" + str1;
+    else if (string_max(str1,str2) == str1){
+        for(int s=1; s>0; s++){
+            count = string_plus(count, "1");
+            str2 = string_plus(str2, "1");
+            if (str2 == str1)
+            {
+                break;
+            }
         }
     }
-    else
-    {
-        length = str1.length();
+    return count;
+}
+
+string string_max(string str1, string str2){
+    string test, max;
+    str1 = string_trimmer(str1);
+    str2 = string_trimmer(str2);
+    if (str1.size() > str2.size()){
+        max = str1;
     }
-    reverse(str1.begin(), str1.end());
-    reverse(str2.begin(), str2.end());
-    for (int o = 0; o < length; o++)
-    {
-        string temp1, temp2, result_temp;
-        temp1 = str1[o];
-        temp2 = str2[o];
-        int temp1int, temp2int;
-        temp1int = stoi(temp1);
-        temp2int = stoi(temp2);
-        if (temp1int < temp2int)
-        {
-            string temp_carry;
-            temp_carry = str1[o+1];
-            temp1 = '1' + temp1;
-            temp_carry = to_string(stoi(temp_carry) - 1);
-            str1[o+1] = temp_carry[0];
+    else if (str2.size() > str1.size()){
+        max = str2;
+    }
+    else if (str2 == str1){
+        max = str1;
+    }
+    else if (str1.size() == str2.size()){
+        string tempstr1, tempstr2;
+        for (int i = 0; i < str1.size(); i++){
+            tempstr1 = str1[i];
+            tempstr2 = str2[i];
+            if (stoi(tempstr1) > stoi(tempstr2)){
+                max = str1;
+                break;
+            }
+            else if (stoi(tempstr1) < stoi(tempstr2)){
+                max = str2;
+                break;
+            }
         }
-        
-        result_temp = to_string(stoi(temp1) - stoi(temp2));
-        result += result_temp;
     }
-    reverse(result.begin(), result.end());
-    return result;
+    return max;
+}
+
+string string_divisor(string divident, string divisor){
+    string divident_slice, temp_quotient, remainder, quotient;
+    if (divident == "0")
+    {
+        return "0";
+    }
+    else if (divisor == "0")
+    {
+        return "Error";
+    }
+    else if (divident == divisor)
+    {
+        return "1";
+    }
+    else if (string_max(divident,divisor) == divisor)
+    {
+        return "0";
+    }
+    else{
+        for(int j = 0; j < divident.size(); j++){
+            divident_slice = remainder + divident_slice + divident[j];
+            remainder = "";
+            divident_slice = string_trimmer(divident_slice);
+            if (string_max(divident_slice, divisor) == divident_slice){
+                temp_quotient = "0";
+                for(int k = 1; k > 0; k++){
+                    if(string_max(string_multiplication(divisor,to_string(k)), divident_slice) == string_multiplication(divisor,to_string(k)) && string_multiplication(divisor,to_string(k)) != divident_slice){
+                        quotient += temp_quotient;
+                        remainder = string_minus(divident_slice, string_multiplication(divisor,to_string(k-1)));
+                        divident_slice = "";
+                        break;
+                    }
+                    temp_quotient = to_string(k);
+                }
+            }
+            else{
+                quotient += "0";
+            }
+        }
+    }
+    return string_trimmer(quotient);
+}
+
+string string_remainder(string divident, string divisor){
+    string quotient = string_divisor(divident,divisor);
+    string product = string_multiplication(quotient,divisor);
+    string remainder = string_minus(divident, product);
+    return remainder;
+}
+
+string string_trimmer(string str){
+    string result_str = "", is_zero;
+    if (str[0] != '0')
+    {
+        return str;
+    }
+    else{
+        for (int i = 0; i < str.size(); i++){
+            is_zero = str[i];
+            if (is_zero == "0")
+            {
+                continue;
+            }
+            else{
+                result_str = str.substr(i,str.size());
+                break;
+            }
+        }
+        if (result_str == "")
+        {
+            result_str = "0";
+        }
+    }
+    return result_str;
 }
 
 //Big Numbers Arithmetic Functions -- End
+
 string dtob(string d){
     int count;
     string result;
-    int a[1000];
+    string a = "";
     count=0;
     for (int i = 0; i >= 0; i++)
     {
         count++;
-        //cout<<string_divisor(d,2)<<"\t"<<stoi(string_remainder(d,2))<<"\t"<<(i+1)<<endl;
-        a[i]=stoi(string_remainder(d,2));
-        d = string_divisor(d,2);
+        a += string_remainder(d,"2");
+        d = string_divisor(d,"2");
         if (d.length()<2)
         {
-            if(stoi(d) < 2){
-                a[i+1]=stoi(d);
+            if(string_max(d, "2") == "2" && d != "2"){
+                a += d;
                 count++;
                 break;
             }
         }
     }
-    for (int j = count; j > 0; j--)
-    {
-        result += to_string(a[j-1]);
-    }
+    reverse(a.begin(), a.end());
+    result = a;
     if(result.length()%4!=0){
         for (int i = 1; i > 0; i++)
         {
@@ -274,101 +358,104 @@ string dtob(string d){
 
 string dtoh(string d){
     int count;
-    string result;
-    string a[1000];
+    string result, temp;
+    string a = "";
     count=0;
     for (int i = 0; i >= 0; i++)
     {
         count++;
-        a[i]=string_remainder(d,16);
-        if (a[i] == "10")
+        temp = string_remainder(d,"16");
+        if (temp == "10")
         {
-            a[i] = "A";
+            a += "A";
         }
-        else if (a[i] == "11")
+        else if (temp == "11")
         {
-            a[i] = "B";
+            a += "B";
         }
-        else if (a[i] == "12")
+        else if (temp == "12")
         {
-            a[i] = "C";
+            a += "C";
         }
-        else if (a[i] == "13")
+        else if (temp == "13")
         {
-            a[i] = "D";
+            a += "D";
         }
-        else if (a[i] == "14")
+        else if (temp == "14")
         {
-            a[i] = "E";
+            a += "E";
         }
-        else if (a[i] == "15")
+        else if (temp == "15")
         {
-            a[i] = "F";
+            a += "F";
         }
-        d = string_divisor(d,16);
+        else
+        {
+            a += temp;
+        }
+        d = string_divisor(d,"16");
         if (d.length()<3)
         {
             if(stoi(d) < 16){
-                a[i+1]=d;
                 count++;
-                if (a[i+1] == "10")
+                temp = d;
+                if (temp == "10")
                 {
-                    a[i+1] = "A";
+                    a += "A";
                 }
-                else if (a[i+1] == "11")
+                else if (temp == "11")
                 {
-                    a[i+1] = "B";
+                    a += "B";
                 }
-                else if (a[i+1] == "12")
+                else if (temp == "12")
                 {
-                    a[i+1] = "C";
+                    a += "C";
                 }
-                else if (a[i+1] == "13")
+                else if (temp == "13")
                 {
-                    a[i+1] = "D";
+                    a += "D";
                 }
-                else if (a[i+1] == "14")
+                else if (temp == "14")
                 {
-                    a[i+1] = "E";
+                    a += "E";
                 }
-                else if (a[i+1] == "15")
+                else if (temp == "15")
                 {
-                    a[i+1] = "F";
+                    a += "F";
+                }
+                else{
+                    a += d;
                 }
                 break;
             }
         }
     }
-    for (int j = count; j > 0; j--)
-    {
-        result += a[j-1];
-    }
-    return result;
+    reverse(a.begin(),a.end());
+    result = a;
+    return string_trimmer(result);
 }
 
 string dtoo(string d){
     int count;
     string result;
-    int a[1000];
+    string a = "";
     count=0;
     for (int i = 0; i >= 0; i++)
     {
         count++;
-        a[i]=stoi(string_remainder(d,8));
-        d = string_divisor(d,8);
+        a += string_remainder(d,"8");
+        d = string_divisor(d,"8");
         if (d.length()<2)
         {
-            if(stoi(d) < 8){
-                a[i+1]=stoi(d);
+            if(string_max(d,"8") == "8" && d != "8"){
+                a += d;
                 count++;
                 break;
             }
         }
     }
-    for (int j = count; j > 0; j--)
-    {
-        result += to_string(a[j-1]);
-    }
+    reverse(a.begin(),a.end());
+    result = a;
     if(result.length()%3!=0){
         for (int i = 1; i > 0; i++)
         {
@@ -386,8 +473,9 @@ string dtoo(string d){
 }
 
 string btod(string d){
-    string temp, result = "", power;
+    string temp, result = "0", power;
     int length;
+    d = string_trimmer(d);
     length = d.size();
     for (int s = 0; s < length; s++)
     {
@@ -399,8 +487,9 @@ string btod(string d){
 }
     
 string otod(string d){
-    string temp, result = "", power;
+    string temp, result = "0", power;
     int length;
+    d = string_trimmer(d);
     length = d.size();
     for (int s = 0; s < length; s++)
     {
@@ -412,8 +501,9 @@ string otod(string d){
 }
 
 string htod(string d){
-    string temp, result = "", power;
+    string temp, result = "0", power;
     int length;
+    d = string_trimmer(d);
     length = d.size();
     for (int s = 0; s < length; s++)
     {
@@ -477,4 +567,5 @@ string htob(string d){
 string htoo(string d){
     return dtoo(htod(d));
 }
+
 //Function Definations -- End
